d967c5d19f2db683dd7387d27502ada24f42f835
Adipiter <miuprix@gmail.com>
2022-09-09 16:05:10 +0700

    delete assets empty
    Signed-off-by: Adipiter <miuprix@gmail.com>

--- src/MainMint.js
+++ src/MainMint.js
@@ -1,14 +1,14 @@
 ﻿import React from 'react';
-import { useState } from 'react';
-import { ethers, BigNumber } from 'ethers';
+import {useState} from 'react';
+import {ethers, BigNumber} from 'ethers';
 import solmintNFT from './SolmintNFT.json';
 
 // style
-import { Box, Button, Flex, Input, Text } from '@chakra-ui/react';
+import {Box, Button, Flex, Input, Text} from '@chakra-ui/react';
 
 const solmintNFTAddress = "0xbD5D53FA86540A27D6aCeCeD551794bb3E0e9e19";
 
-const MainMint = ({ accounts, setAccounts }) => {
+const MainMint = ({accounts, setAccounts}) => {
 
     // num of quant user is selecting
     const [mintAmount, setMintAmount] = useState(1);
@@ -17,18 +17,18 @@ const MainMint = ({ accounts, setAccounts }) => {
     async function handleMint() {
         if (window.ethereum) {
             // connect to blockchain
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const provider = new ethers
+                .providers
+                .Web3Provider(window.ethereum);
             const signer = provider.getSigner();
-            const contract = new ethers.Contract(
-                solmintNFTAddress,
-                solmintNFT.abi,
-                signer
-            );
+            const contract = new ethers.Contract(solmintNFTAddress, solmintNFT.abi, signer);
 
             try {
                 // actuall smart contract call -> eth needs big numbers
                 const response = await contract.mint(BigNumber.from(mintAmount), {
-                    value: ethers.utils.parseEther((0.02 * mintAmount).toString()),
+                    value: ethers
+                        .utils
+                        .parseEther((0.02 * mintAmount).toString())
                 });
 
                 console.log("✔ response: ", response);
@@ -39,40 +39,74 @@ const MainMint = ({ accounts, setAccounts }) => {
     }
 
     const handleDecrement = () => {
-        if (mintAmount <= 1) return;
-
+        if (mintAmount <= 1) 
+            return;
+        
         setMintAmount(mintAmount - 1);
     }
 
     const handleIncrement = () => {
-        if (mintAmount >= 3) return;
-
+        if (mintAmount >= 3) 
+            return;
+        
         setMintAmount(mintAmount + 1);
     }
 
     return (
         <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
             <Box width="520px">
-
                 <div>
                     <Text fontSize="48px" textShadow="0 5px #000000">RoboPunks</Text>
                     <Text
                         fontSize="30px"
                         letterSpacing="-5.5%"
                         fontFamily="VT323"
-                        textShadow="0 2px 2px #000000"
-                    >
-                        It's 2100.
-                        Can the RoboPunks NFT save humans from destructive scam NFTs?
-                        Mint RoboPunks to find out.
+                        textShadow="0 2px 2px #000000">
+                        It's 2100. Can the RoboPunks NFT save humans from destructive scam NFTs? Mint
+                        RoboPunks to find out.
                     </Text>
                 </div>
 
-                {isConnected ? (
-
-                    <div>
-                  <Flex align="center" justify="center">
-                            <Button 
+                {
+                    isConnected
+                        ? (<div>
+                            <Flex align="center" justify="center">
+                                <Button
+                                    backgroundColor="#D6517D"
+                                    borderRadius="5px"
+                                    boxShadow="0px 2px 2px 1px #0F0F0F"
+                                    color="white"
+                                    cursor="pointer"
+                                    fontFamily="inherit"
+                                    padding="15px"
+                                    marginTop="10px"
+                                    onClick={handleDecrement}>
+                                    -
+                                </Button>
+                                <Input
+                                    readOnly="readOnly"
+                                    fontFamily="inherit"
+                                    width="100px"
+                                    height="40px"
+                                    textAlign="center"
+                                    paddingLeft="19px"
+                                    marginTop="10px"
+                                    type="number"
+                                    value={mintAmount}/>
+                                <Button
+                                    backgroundColor="#D6517D"
+                                    borderRadius="5px"
+                                    boxShadow="0px 2px 2px 1px #0F0F0F"
+                                    color="white"
+                                    cursor="pointer"
+                                    fontFamily="inherit"
+                                    padding="15px"
+                                    marginTop="10px"
+                                    onClick={handleIncrement}>
+                                    +
+                                </Button>
+                            </Flex>
+                            <Button
                                 backgroundColor="#D6517D"
                                 borderRadius="5px"
                                 boxShadow="0px 2px 2px 1px #0F0F0F"
@@ -81,62 +115,23 @@ const MainMint = ({ accounts, setAccounts }) => {
                                 fontFamily="inherit"
                                 padding="15px"
                                 marginTop="10px"
-                                onClick={handleDecrement}
-                            >
-                                -
+                                onClick={handleMint}>
+                                Mint Now
                             </Button>
-                            <Input 
-                                readOnly
-                                fontFamily="inherit"
-                                width="100px"
-                                height="40px"
-                                textAlign="center"
-                                paddingLeft="19px"
-                                marginTop="10px"
-                                type="number"
-                                value={mintAmount}
-                            />
-                            <Button 
-                                backgroundColor="#D6517D"
-                                borderRadius="5px"
-                                boxShadow="0px 2px 2px 1px #0F0F0F"
-                                color="white"
-                                cursor="pointer"
-                                fontFamily="inherit"
-                                padding="15px"
-                                marginTop="10px"
-                                onClick={handleIncrement}
-                            >
-                                +
-                            </Button>
-                        </Flex>
-                        <Button 
-                            backgroundColor="#D6517D"
-                            borderRadius="5px"
-                            boxShadow="0px 2px 2px 1px #0F0F0F"
-                            color="white"
-                            cursor="pointer"
-                            fontFamily="inherit"
-                            padding="15px"
-                            marginTop="10px"
-                            onClick={handleMint}
-                        >
-                            Mint Now
-                        </Button>
 
-                        ) : (
-                        <Text
-                            marginTop="70px"
-                            fontSize="30px"
-                            letterSpacing="-5.5%"
-                            fontFamily="VT323"
-                            textShadow="0 2px 2px #000000"
-                            color="#D6517D"
-                        >
-                            You Must Connect To Mint.
-                        </Text>
-                )}
-                    </Box>
+                            ) : (
+                            <Text
+                                marginTop="70px"
+                                fontSize="30px"
+                                letterSpacing="-5.5%"
+                                fontFamily="VT323"
+                                textShadow="0 2px 2px #000000"
+                                color="#D6517D">
+                                You Must Connect To Mint.
+                            </Text>
+                            )
+                }
+            </Box>
         </Flex>
     )
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,81 +1,75 @@
-import { Box, Button, Flex, Image, Link, Spacer } from '@chakra-ui/react';
-import FacebookIcon from './assets/social-media-icons/facebook_32x32.png';
-import TwitterIcon from './assets/social-media-icons/twitter_32x32.png';
-import EmailIcon from './assets/social-media-icons/email_32x32.png'; 
+import { Box, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react";
 
 const NavBar = ({ accounts, setAccounts }) => {
-    const isConnected = Boolean(accounts[0]);
+  const isConnected = Boolean(accounts[0]);
 
-    async function connectAccount() {
-        const isMetaMask = isMetaMaskInstalled();
-        if (!isMetaMask) { return }
-
-        if (window.ethereum) {
-            const accounts = await window.ethereum.request({
-                method: "eth_requestAccounts",
-            });
-            setAccounts(accounts);
-        }
+  async function connectAccount() {
+    const isMetaMask = isMetaMaskInstalled();
+    if (!isMetaMask) {
+      return;
     }
 
-    function isMetaMaskInstalled() {
-        return Boolean(window.ethereum && window.ethereum.isMetaMask);
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccounts(accounts);
     }
+  }
 
-    return (
-        <Flex justify="space-between" align="center" padding="30px">
-            {/* Left Side - Social Media Icons */}
-            <Flex justify="space-around" width="40%" padding="0 75px">
-                <Link href='https://www.facebook.com'>
-                    <Image src={FacebookIcon} boxSize="42px" margin="0 15px" />
-                </Link>
+  function isMetaMaskInstalled() {
+    return Boolean(window.ethereum && window.ethereum.isMetaMask);
+  }
 
-                <Link href='https://www.twitter.com'>
-                    <Image src={TwitterIcon} boxSize="42px" margin="0 15px" />
-                </Link>
+  return (
+    <Flex justify="space-between" align="center" padding="30px">
+      {/* Left Side - Social Media Icons */}
+      <Flex justify="space-around" width="40%" padding="0 75px">
+        <Link href="https://www.facebook.com">
+          <Image src={FacebookIcon} boxSize="42px" margin="0 15px" />
+        </Link>
 
-                <Link href='https://www.gmail.com'>
-                    <Image src={EmailIcon} boxSize="42px" margin="0 15px" />
-                </Link>
-            </Flex>
-            {/* Right Side - Sections And Connect */}
-            <Flex
-                justify="space-around"
-                align="center"
-                width="40%"
-                padding="30px"
+        <Link href="https://www.twitter.com">
+          <Image src={TwitterIcon} boxSize="42px" margin="0 15px" />
+        </Link>
 
-            >
-                <Box margin="0 15px">About</Box>
-                <Spacer />
-                <Box margin="0 15px">Mint</Box>
-                <Spacer />
-                <Box margin="0 15px">Team</Box>
-                <Spacer />
+        <Link href="https://www.gmail.com">
+          <Image src={EmailIcon} boxSize="42px" margin="0 15px" />
+        </Link>
+      </Flex>
+      {/* Right Side - Sections And Connect */}
+      <Flex justify="space-around" align="center" width="40%" padding="30px">
+        <Box margin="0 15px">About</Box>
+        <Spacer />
+        <Box margin="0 15px">Mint</Box>
+        <Spacer />
+        <Box margin="0 15px">Team</Box>
+        <Spacer />
 
-                {/*Connect Wallet*/}
-                {isConnected ? (
-                    <div>
-                        <Box margin="0 15px">{accounts[0].slice(0, 6)}...{accounts[0].slice(39)}</Box>
-                    </div>
-                ) : (
-                    <Button
-                        backgroundColor="#D6517D"
-                        borderRadius="5px"
-                        boxShadow="0px 2px 2px 1px #0F0F0F"
-                        color="white"
-                        cursor="pointer"
-                        fontFamily="inherit"
-                        padding="15px"
-                        margin="0 15px"
-                        onClick={connectAccount}
-                    >
-                        Connect Wallet
-                    </Button>
-                )}
-
-            </Flex>
-        </Flex>
-    )
-}
-export default NavBar
+        {/*Connect Wallet*/}
+        {isConnected ? (
+          <div>
+            <Box margin="0 15px">
+              {accounts[0].slice(0, 6)}...{accounts[0].slice(39)}
+            </Box>
+          </div>
+        ) : (
+          <Button
+            backgroundColor="#D6517D"
+            borderRadius="5px"
+            boxShadow="0px 2px 2px 1px #0F0F0F"
+            color="white"
+            cursor="pointer"
+            fontFamily="inherit"
+            padding="15px"
+            margin="0 15px"
+            onClick={connectAccount}
+          >
+            Connect Wallet
+          </Button>
+        )}
+      </Flex>
+    </Flex>
+  );
+};
+export default NavBar;
